8078651f0cd568c8415973cf666e954bb8602fdb
leewg97 <leewg97@naver.com>
2022-11-02 03:29:24 +0900

    Setup Spring Boot

---
+++ src/main/java/com/sb/entrancecounting/EntranceCountingServiceApplication.java
@@ -0,0 +1,13 @@
+package com.sb.entrancecounting;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EntranceCountingServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EntranceCountingServiceApplication.class, args);
+    }
+
+}
