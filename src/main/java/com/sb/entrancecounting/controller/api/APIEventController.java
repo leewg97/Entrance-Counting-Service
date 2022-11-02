99e1311872f08927061557315672f2d4e2f40c44
leewg97 <leewg97@naver.com>
2022-11-03 04:04:57 +0900

    Add #2 - APIEventController, DTO, Tests - 아직 서비스로직이 없으므로 dummy 데이터를 컨트롤러에 임시로 사용 - 서비스 구현할 때 삭제할 예정

--- src/main/java/com/sb/entrancecounting/controller/api/APIEventController.java
+++ src/main/java/com/sb/entrancecounting/controller/api/APIEventController.java
@@ -1,8 +1,13 @@
 package com.sb.entrancecounting.controller.api;
 
-import com.sb.entrancecounting.exception.GeneralException;
+import com.sb.entrancecounting.constant.EventStatus;
+import com.sb.entrancecounting.dto.APIDataResponse;
+import com.sb.entrancecounting.dto.EventRequest;
+import com.sb.entrancecounting.dto.EventResponse;
+import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
 
+import java.time.LocalDateTime;
 import java.util.List;
 
 @RestController
@@ -10,28 +15,53 @@ import java.util.List;
 public class APIEventController {
 
     @GetMapping("/events")
-    public List<String> getEvents() {
-        return List.of("event1", "event2");
+    public APIDataResponse<List<EventResponse>> getEvents() {
+        return APIDataResponse.of(List.of(EventResponse.of(
+                1L,
+                "오후 운동",
+                EventStatus.OPENED,
+                LocalDateTime.of(2022, 11, 3, 19, 0, 0),
+                LocalDateTime.of(2022, 11, 3, 21, 0, 0),
+                0,
+                24,
+                "스트랩 챙겨요"
+        )));
     }
 
+
+    @ResponseStatus(HttpStatus.CREATED)
     @PostMapping("/events")
-    public Boolean createEvent() {
-        return true;
+    public APIDataResponse<Void> createEvent(@RequestBody EventRequest eventRequest) {
+        return APIDataResponse.empty();
     }
 
     @GetMapping("/events/{eventId}")
-    public String getEvent(@PathVariable Integer eventId) {
-        return "place" + eventId;
+    public APIDataResponse<EventResponse> getEvent(@PathVariable Long eventId) {
+        if (eventId.equals(2L)) {
+            return APIDataResponse.empty();
+        }
+
+        return APIDataResponse.of(EventResponse.of(
+                1L,
+                "오후 운동",
+                EventStatus.OPENED,
+                LocalDateTime.of(2022, 11, 3, 19, 0, 0),
+                LocalDateTime.of(2022, 11, 3, 21, 0, 0),
+                0,
+                24,
+                "스트랩 챙겨요"
+        ));
     }
 
     @PutMapping("/events/{eventId}")
-    public Boolean modifyEvent(@PathVariable Integer eventId) {
-        return true;
+    public APIDataResponse<Void> modifyEvent(@PathVariable Long eventId, @RequestBody EventRequest eventRequest) {
+        return APIDataResponse.empty();
     }
 
     @DeleteMapping("/events/{eventId}")
-    public Boolean removeEvent(@PathVariable Integer eventId) {
-        return true;
+    public APIDataResponse<Void> removeEvent(@PathVariable Long eventId) {
+        return APIDataResponse.empty();
     }
+
 
 }
---
+++ src/main/java/com/sb/entrancecounting/dto/EventResponse.java
@@ -0,0 +1,38 @@
+package com.sb.entrancecounting.dto;
+
+import com.sb.entrancecounting.constant.EventStatus;
+
+import java.time.LocalDateTime;
+
+public record EventResponse(
+        Long placeId,
+        String eventName,
+        EventStatus eventStatus,
+        LocalDateTime eventStartDatetime,
+        LocalDateTime eventEndDatetime,
+        Integer currentNumberOfPeople,
+        Integer capacity,
+        String memo
+) {
+    public static EventResponse of(
+            Long placeId,
+            String eventName,
+            EventStatus eventStatus,
+            LocalDateTime eventStartDatetime,
+            LocalDateTime eventEndDatetime,
+            Integer currentNumberOfPeople,
+            Integer capacity,
+            String memo
+    ) {
+        return new EventResponse(
+                placeId,
+                eventName,
+                eventStatus,
+                eventStartDatetime,
+                eventEndDatetime,
+                currentNumberOfPeople,
+                capacity,
+                memo
+        );
+    }
+}
