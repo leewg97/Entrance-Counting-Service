66c76317e5bf5ca093ece74ea5c9578ac1570955
leewg97 <leewg97@naver.com>
2022-11-03 01:52:23 +0900

    Implement #2 - Api Controller Advice - API 전용 `@RestControllerAdvice` 구현 - Spring MVC Exception 처리 위해 `ResponseEntityExceptionHandler` 상속 및 오버라이딩 코드 추가

---
+++ src/main/java/com/sb/entrancecounting/controller/error/APIExceptionHandler.java
@@ -0,0 +1,72 @@
+package com.sb.entrancecounting.controller.error;
+
+import com.sb.entrancecounting.constant.ErrorCode;
+import com.sb.entrancecounting.dto.APIErrorResponse;
+import com.sb.entrancecounting.exception.GeneralException;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+import org.springframework.web.context.request.WebRequest;
+import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
+
+@RestControllerAdvice(annotations = RestController.class)
+public class APIExceptionHandler extends ResponseEntityExceptionHandler {
+
+    /*
+     * GeneralException 의 형태에 따라 바뀌게끔 해석
+     */
+    @ExceptionHandler
+    public ResponseEntity<Object> general(GeneralException e, WebRequest request) {
+        ErrorCode errorCode = e.getErrorCode();
+        HttpStatus status = errorCode.isClientSideError() ?
+                HttpStatus.BAD_REQUEST :
+                HttpStatus.INTERNAL_SERVER_ERROR;
+
+        return super.handleExceptionInternal(
+                e,
+                APIErrorResponse.of(false, errorCode.getCode(), errorCode.getMessage(e)),
+                HttpHeaders.EMPTY,
+                status,
+                request
+        );
+    }
+
+    /*
+    * 예상치 못한 일반적인 Error
+    */
+    @ExceptionHandler
+    public ResponseEntity<Object> exception(Exception e, WebRequest request) {
+        ErrorCode errorCode = ErrorCode.INTERNAL_ERROR;
+        HttpStatus status = HttpStatus.INTERNAL_SERVER_ERROR;
+
+        return super.handleExceptionInternal(
+                e,
+                APIErrorResponse.of(false, errorCode.getCode(), errorCode.getMessage(e)),
+                HttpHeaders.EMPTY,
+                status,
+                request
+        );
+    }
+
+    /*
+     * Spring Error
+     */
+    @Override
+    protected ResponseEntity<Object> handleExceptionInternal(Exception ex, Object body, HttpHeaders headers,
+                                                             HttpStatus status, WebRequest request) {
+        ErrorCode errorCode = status.is4xxClientError() ?
+                ErrorCode.SPRING_BAD_REQUEST :
+                ErrorCode.SPRING_INTERNAL_ERROR;
+
+        return super.handleExceptionInternal(
+                ex,
+                APIErrorResponse.of(false, errorCode.getCode(), errorCode.getMessage(ex)),
+                headers,
+                status,
+                request
+        );
+    }
+}
