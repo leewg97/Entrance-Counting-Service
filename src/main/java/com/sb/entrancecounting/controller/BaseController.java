2e30ce1ba123b0019f637110c53a1066e5fd8491
leewg97 <leewg97@naver.com>
2022-11-03 01:28:49 +0900

    Update #2 - BaseController - BaseController에서 에러 처리 분리

--- src/main/java/com/sb/entrancecounting/controller/BaseController.java
+++ src/main/java/com/sb/entrancecounting/controller/BaseController.java
@@ -1,21 +1,14 @@
 package com.sb.entrancecounting.controller;
 
-import org.springframework.boot.web.servlet.error.ErrorController;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
-public class BaseController implements ErrorController {
+public class BaseController {
 
     @GetMapping("/")
     public String index() {
         return "index";
-    }
-
-    @RequestMapping("/error")
-    public String error() {
-        return "error";
     }
 
 }
