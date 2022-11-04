a52c2a860fb19c9cf5b9a2599c1d58531e7d1117
leewg97 <leewg97@naver.com>
2022-11-05 06:44:10 +0900

    Update #8 - EventController - custom view 추가

--- src/main/java/com/sb/entrancecounting/controller/EventController.java
+++ src/main/java/com/sb/entrancecounting/controller/EventController.java
@@ -2,22 +2,28 @@ package com.sb.entrancecounting.controller;
 
 import com.querydsl.core.types.Predicate;
 import com.sb.entrancecounting.constant.ErrorCode;
+import com.sb.entrancecounting.constant.EventStatus;
 import com.sb.entrancecounting.domain.Event;
 import com.sb.entrancecounting.dto.EventResponse;
+import com.sb.entrancecounting.dto.EventViewResponse;
 import com.sb.entrancecounting.exception.GeneralException;
 import com.sb.entrancecounting.service.EventService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.querydsl.binding.QuerydslPredicate;
+import org.springframework.format.annotation.DateTimeFormat;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
 
+import javax.validation.constraints.Size;
+import java.time.LocalDateTime;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
-import java.util.stream.Collectors;
 
 @Controller
 @RequiredArgsConstructor
@@ -32,8 +38,34 @@ public class EventController {
         List<EventResponse> events = eventService.getEvents(predicate)
                 .stream()
                 .map(EventResponse::from)
-                .collect(Collectors.toList());
+                .toList();
+
         map.put("events", events);
+
+        return new ModelAndView("event/index", map);
+    }
+
+    @GetMapping("/custom")
+    public ModelAndView customEvents(
+            @Size(min = 2) String placeName,
+            @Size(min = 2) String eventName,
+            EventStatus eventStatus,
+            @DateTimeFormat(iso = DateTimeFormat.ISO.DATE_TIME) LocalDateTime eventStartDatetime,
+            @DateTimeFormat(iso = DateTimeFormat.ISO.DATE_TIME) LocalDateTime eventEndDatetime,
+            Pageable pageable
+    ) {
+        Map<String, Object> map = new HashMap<>();
+        Page<EventViewResponse> events = eventService.getEventViewResponse(
+                placeName,
+                eventName,
+                eventStatus,
+                eventStartDatetime,
+                eventEndDatetime,
+                pageable
+        );
+
+        map.put("events", events);
+
         return new ModelAndView("event/index", map);
     }
 
@@ -43,9 +75,10 @@ public class EventController {
         EventResponse event = eventService.getEvent(eventId)
                 .map(EventResponse::from)
                 .orElseThrow(() -> new GeneralException(ErrorCode.NOT_FOUND));
+
         map.put("event", event);
+
         return new ModelAndView("event/detail", map);
     }
-
 
 }
