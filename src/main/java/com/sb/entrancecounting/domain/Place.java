ffebf71f61f6c16159131756581a04370c105fae
leewg97 <leewg97@naver.com>
2022-11-04 21:58:28 +0900

    Update #7 - Place - columnDefinition 수정 및 연관관계 매핑 추가

--- src/main/java/com/sb/entrancecounting/domain/Place.java
+++ src/main/java/com/sb/entrancecounting/domain/Place.java
@@ -1,7 +1,6 @@
 package com.sb.entrancecounting.domain;
 
 import com.sb.entrancecounting.constant.PlaceType;
-import lombok.EqualsAndHashCode;
 import lombok.Getter;
 import lombok.Setter;
 import lombok.ToString;
@@ -11,10 +10,12 @@ import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 
 import javax.persistence.*;
 import java.time.LocalDateTime;
+import java.util.LinkedHashSet;
+import java.util.Objects;
+import java.util.Set;
 
 @Getter
 @ToString
-@EqualsAndHashCode
 @Table(indexes = {
         @Index(columnList = "placeName"),
         @Index(columnList = "address"),
@@ -30,9 +31,8 @@ public class Place {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
-
     @Setter
-    @Column(nullable = false, columnDefinition = "varchar default 'COMMON'")
+    @Column(nullable = false, columnDefinition = "varchar(20) default 'COMMON'")
     @Enumerated(EnumType.STRING)
     private PlaceType placeType;
 
@@ -52,10 +52,8 @@ public class Place {
     @Column(nullable = false, columnDefinition = "integer default 0")
     private Integer capacity;
 
-
     @Setter
     private String memo;
-
 
     @Column(nullable = false, insertable = false, updatable = false,
             columnDefinition = "datetime default CURRENT_TIMESTAMP")
@@ -67,6 +65,15 @@ public class Place {
     @LastModifiedDate
     private LocalDateTime modifiedAt;
 
+    @ToString.Exclude
+    @OrderBy("id")
+    @OneToMany(mappedBy = "place")
+    private final Set<Event> events = new LinkedHashSet<>();
+
+    @ToString.Exclude
+    @OrderBy("id")
+    @OneToMany(mappedBy = "place")
+    private final Set<AdminPlaceMap> adminPlaceMaps = new LinkedHashSet<>();
 
     protected Place() {}
 
@@ -95,6 +102,18 @@ public class Place {
             String memo
     ) {
         return new Place(placeType, placeName, address, phoneNumber, capacity, memo);
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj) return true;
+        if (obj == null || getClass() != obj.getClass()) return false;
+        return id != null && id.equals(((Place) obj).getId());
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(placeName, address, phoneNumber, createdAt, modifiedAt);
     }
 
 }
