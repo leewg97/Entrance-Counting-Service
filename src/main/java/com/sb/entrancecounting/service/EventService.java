00ec92e0a65a13ef3c9f27020e57316a1a237f53
leewg97 <leewg97@naver.com>
2022-11-05 06:45:38 +0900

    Update #8 - EventService, PlaceService - Querydsl 적용 후 로직 수정

--- src/main/java/com/sb/entrancecounting/service/EventService.java
+++ src/main/java/com/sb/entrancecounting/service/EventService.java
@@ -2,39 +2,103 @@ package com.sb.entrancecounting.service;
 
 import com.querydsl.core.types.Predicate;
 import com.sb.entrancecounting.constant.ErrorCode;
+import com.sb.entrancecounting.constant.EventStatus;
+import com.sb.entrancecounting.domain.Event;
 import com.sb.entrancecounting.domain.Place;
 import com.sb.entrancecounting.dto.EventDto;
+import com.sb.entrancecounting.dto.EventViewResponse;
 import com.sb.entrancecounting.exception.GeneralException;
 import com.sb.entrancecounting.repository.EventRepository;
 import com.sb.entrancecounting.repository.PlaceRepository;
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageImpl;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
+import java.time.LocalDateTime;
 import java.util.List;
 import java.util.Optional;
-import java.util.stream.Collectors;
 import java.util.stream.StreamSupport;
 
 @Service
+@Transactional
 @RequiredArgsConstructor
 public class EventService {
 
     private final EventRepository eventRepository;
     private final PlaceRepository placeRepository;
 
+    @Transactional(readOnly = true)
     public List<EventDto> getEvents(Predicate predicate) {
         try {
             return StreamSupport.stream(eventRepository.findAll(predicate).spliterator(), false)
                     .map(EventDto::of)
-                    .collect(Collectors.toList());
+                    .toList();
         } catch (Exception e) {
             throw new GeneralException(ErrorCode.DATA_ACCESS_ERROR, e);
         }
     }
 
+    @Transactional(readOnly = true)
+    public Page<EventViewResponse> getEventViewResponse(
+            String placeName,
+            String eventName,
+            EventStatus eventStatus,
+            LocalDateTime eventStartDatetime,
+            LocalDateTime eventEndDatetime,
+            Pageable pageable
+    ) {
+        try {
+            return eventRepository.findEventViewPageBySearchParams(
+                    placeName,
+                    eventName,
+                    eventStatus,
+                    eventStartDatetime,
+                    eventEndDatetime,
+                    pageable
+            );
+        } catch (Exception e) {
+            throw new GeneralException(ErrorCode.DATA_ACCESS_ERROR, e);
+        }
+    }
+
+    @Transactional(readOnly = true)
     public Optional<EventDto> getEvent(Long eventId) {
         try {
             return eventRepository.findById(eventId).map(EventDto::of);
+        } catch (Exception e) {
+            throw new GeneralException(ErrorCode.DATA_ACCESS_ERROR, e);
+        }
+    }
+
+    @Transactional(readOnly = true)
+    public Page<EventViewResponse> getEvent(Long placeId, Pageable pageable) {
+        try {
+            Place place = placeRepository.getById(placeId);
+            Page<Event> eventPage = eventRepository.findByPlace(place, pageable);
+
+            return new PageImpl<>(
+                    eventPage.getContent()
+                            .stream()
+                            .map(event -> EventViewResponse.from(EventDto.of(event)))
+                            .toList(),
+                    eventPage.getPageable(),
+                    eventPage.getTotalElements()
+            );
+        } catch (Exception e) {
+            throw new GeneralException(ErrorCode.DATA_ACCESS_ERROR, e);
+        }
+    }
+
+    public boolean upsertEvent(EventDto eventDto) {
+        try {
+            if (eventDto.id() != null) {
+                return modifyEvent(eventDto.id(), eventDto);
+            } else {
+                return createEvent(eventDto);
+            }
         } catch (Exception e) {
             throw new GeneralException(ErrorCode.DATA_ACCESS_ERROR, e);
         }
@@ -46,9 +110,7 @@ public class EventService {
                 return false;
             }
 
-            Place place = placeRepository.findById(eventDto.placeDto().id()).orElseThrow(
-                    () -> new GeneralException(ErrorCode.NOT_FOUND)
-            );
+            Place place = placeRepository.getById(eventDto.placeDto().id());
             eventRepository.save(eventDto.toEntity(place));
             return true;
         } catch (Exception e) {
@@ -56,13 +118,15 @@ public class EventService {
         }
     }
 
-    public boolean modifyEvent(Long eventId, EventDto eventDto) {
+    public boolean modifyEvent(Long eventId, EventDto dto) {
         try {
-            if (eventId == null || eventDto == null) {
+            if (eventId == null || dto == null) {
                 return false;
             }
+
             eventRepository.findById(eventId)
-                    .ifPresent(event -> eventRepository.save(eventDto.updateEntity(event)));
+                    .ifPresent(event -> eventRepository.save(dto.updateEntity(event)));
+
             return true;
         } catch (Exception e) {
             throw new GeneralException(ErrorCode.DATA_ACCESS_ERROR, e);
@@ -74,11 +138,11 @@ public class EventService {
             if (eventId == null) {
                 return false;
             }
+
             eventRepository.deleteById(eventId);
             return true;
         } catch (Exception e) {
             throw new GeneralException(ErrorCode.DATA_ACCESS_ERROR, e);
         }
     }
-
 }
