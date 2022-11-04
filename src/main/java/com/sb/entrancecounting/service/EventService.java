caf4e3479198640228c735e845703a838d7949ce
leewg97 <leewg97@naver.com>
2022-11-04 21:56:56 +0900

    Update #7 - EventService & EventServiceTest - Place와 연관관계 매핑 추가에 따라 로직 수정

--- src/main/java/com/sb/entrancecounting/service/EventService.java
+++ src/main/java/com/sb/entrancecounting/service/EventService.java
@@ -2,14 +2,14 @@ package com.sb.entrancecounting.service;
 
 import com.querydsl.core.types.Predicate;
 import com.sb.entrancecounting.constant.ErrorCode;
-import com.sb.entrancecounting.constant.EventStatus;
+import com.sb.entrancecounting.domain.Place;
 import com.sb.entrancecounting.dto.EventDto;
 import com.sb.entrancecounting.exception.GeneralException;
 import com.sb.entrancecounting.repository.EventRepository;
+import com.sb.entrancecounting.repository.PlaceRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
-import java.time.LocalDateTime;
 import java.util.List;
 import java.util.Optional;
 import java.util.stream.Collectors;
@@ -20,6 +20,7 @@ import java.util.stream.StreamSupport;
 public class EventService {
 
     private final EventRepository eventRepository;
+    private final PlaceRepository placeRepository;
 
     public List<EventDto> getEvents(Predicate predicate) {
         try {
@@ -45,7 +46,10 @@ public class EventService {
                 return false;
             }
 
-            eventRepository.save(eventDto.toEntity());
+            Place place = placeRepository.findById(eventDto.placeDto().id()).orElseThrow(
+                    () -> new GeneralException(ErrorCode.NOT_FOUND)
+            );
+            eventRepository.save(eventDto.toEntity(place));
             return true;
         } catch (Exception e) {
             throw new GeneralException(ErrorCode.DATA_ACCESS_ERROR, e);
