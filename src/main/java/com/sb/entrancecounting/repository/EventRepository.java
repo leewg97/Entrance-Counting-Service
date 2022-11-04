2bfb171a299c2296df1d68ef1f36883643656537
leewg97 <leewg97@naver.com>
2022-11-05 01:17:25 +0900

    Update #7 - EventRepository - EventRepositoryCustom extends

--- src/main/java/com/sb/entrancecounting/repository/EventRepository.java
+++ src/main/java/com/sb/entrancecounting/repository/EventRepository.java
@@ -4,17 +4,23 @@ import com.querydsl.core.types.dsl.ComparableExpression;
 import com.querydsl.core.types.dsl.StringExpression;
 import com.sb.entrancecounting.domain.Event;
 import com.sb.entrancecounting.domain.QEvent;
+import com.sb.entrancecounting.repository.querydsl.EventRepositoryCustom;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.querydsl.QuerydslPredicateExecutor;
 import org.springframework.data.querydsl.binding.QuerydslBinderCustomizer;
 import org.springframework.data.querydsl.binding.QuerydslBindings;
 
-public interface EventRepository extends JpaRepository<Event, Long>, QuerydslPredicateExecutor<Event>, QuerydslBinderCustomizer<QEvent> {
+public interface EventRepository extends
+        JpaRepository<Event, Long>,
+        EventRepositoryCustom,
+        QuerydslPredicateExecutor<Event>,
+        QuerydslBinderCustomizer<QEvent> {
 
     @Override
     default void customize(QuerydslBindings bindings, QEvent root) {
         bindings.excludeUnlistedProperties(true);
         bindings.including(root.place.placeName, root.eventName, root.eventStatus, root.eventStartDatetime, root.eventEndDatetime);
+        bindings.bind(root.place.placeName).first(StringExpression::containsIgnoreCase);
         bindings.bind(root.eventName).first(StringExpression::containsIgnoreCase);
         bindings.bind(root.eventStartDatetime).first(ComparableExpression::goe);
         bindings.bind(root.eventEndDatetime).first(ComparableExpression::loe);
