c9ef11e4078b7e87e41a4d6d20d5866145efb759
leewg97 <leewg97@naver.com>
2022-11-05 06:35:14 +0900

    Update #8 - BaseController - redirect url 변경

--- src/main/java/com/sb/entrancecounting/controller/BaseController.java
+++ src/main/java/com/sb/entrancecounting/controller/BaseController.java
@@ -7,8 +7,8 @@ import org.springframework.web.bind.annotation.GetMapping;
 public class BaseController {
 
     @GetMapping("/")
-    public String index() {
-        return "index";
+    public String root() {
+        return "redirect:/events";
     }
 
 }
