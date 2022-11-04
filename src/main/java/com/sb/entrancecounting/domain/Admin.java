8b6472f7c71fd200c09ef7846c25353ccac860d1
leewg97 <leewg97@naver.com>
2022-11-04 21:48:40 +0900

    Update #7 - Admin Entity - `AdminPlaceMaps`엔티티와 `OneToMany`

--- src/main/java/com/sb/entrancecounting/domain/Admin.java
+++ src/main/java/com/sb/entrancecounting/domain/Admin.java
@@ -1,6 +1,5 @@
 package com.sb.entrancecounting.domain;
 
-import lombok.EqualsAndHashCode;
 import lombok.Getter;
 import lombok.Setter;
 import lombok.ToString;
@@ -10,10 +9,12 @@ import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 
 import javax.persistence.*;
 import java.time.LocalDateTime;
+import java.util.LinkedHashSet;
+import java.util.Objects;
+import java.util.Set;
 
 @Getter
 @ToString
-@EqualsAndHashCode
 @Table(indexes = {
         @Index(columnList = "phoneNumber"),
         @Index(columnList = "createdAt"),
@@ -26,7 +27,6 @@ public class Admin {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
-
 
     @Setter
     @Column(nullable = false, unique = true)
@@ -44,10 +44,14 @@ public class Admin {
     @Column(nullable = false)
     private String phoneNumber;
 
-
     @Setter
     private String memo;
 
+
+    @ToString.Exclude
+    @OrderBy("id")
+    @OneToMany(mappedBy = "admin")
+    private final Set<AdminPlaceMap> adminPlaceMaps = new LinkedHashSet<>();
 
     @Column(nullable = false, insertable = false, updatable = false,
             columnDefinition = "datetime default CURRENT_TIMESTAMP")
@@ -58,7 +62,6 @@ public class Admin {
             columnDefinition = "datetime default CURRENT_TIMESTAMP on update CURRENT_TIMESTAMP")
     @LastModifiedDate
     private LocalDateTime modifiedAt;
-
 
     protected Admin() {}
 
@@ -72,6 +75,18 @@ public class Admin {
 
     public static Admin of(String email, String nickname, String password, String phoneNumber, String memo) {
         return new Admin(email, nickname, password, phoneNumber, memo);
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj) return true;
+        if (obj == null || getClass() != obj.getClass()) return false;
+        return id != null && id.equals(((Admin) obj).getId());
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(email, nickname, phoneNumber, createdAt, modifiedAt);
     }
 
 }
